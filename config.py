923aa9f2c3270449e83c562b4b0306292e5a4ccc
PrinceOfPuppers <joshuamcpherson5@gmail.com>
2019-06-28 06:43:13 -0400

    added new mode for inputting point lists

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ class Config:
     def __init__(self):
         #all in pixel counts
         self.screenSize=[1500,800]
-        self.origin=[50,50]
+        self.origin=[750,400]
+
+        #0 for line list, 1 for point list
+        self.mode=0
 
         #functions as a scaling factor so you can more accuratly place points
         #point lists will be outputted as if each grid intersection was one pixel
@@ -14,6 +17,10 @@ class Config:
         self.displayPrevious=False
 
         #creates a dict containing each letter of the alphabet
-        self.convertToFontDict=True
+        self.convertToFontDict=False
         self.alphabet=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ','1','2']
+
+        #displays guiding lines for graphing
+        self.lineListGraphGuides=[]
+        self.pointListGraphGuides=[]
 config=Config()
--- point-input-tool.py
+++ point-input-tool.py
@@ -33,6 +33,7 @@ def drawGrid(gameDisplay):
     pg.draw.aaline(gameDisplay,(0,0,255),point1,point2)
     
     #yGridLines
+    #positive
     loopNumber=int(round((screenSize[0]-config.origin[0])/config.gridSpaceing))
     for lineValue in range(1,loopNumber+1):
         lineX=config.origin[0]+config.gridSpaceing*lineValue
@@ -41,16 +42,34 @@ def drawGrid(gameDisplay):
         point2=(lineX,config.screenSize[1])
         pg.draw.aaline(gameDisplay,(255,0,0),point1,point2)
 
-    
+    #negative
+    loopNumber=int(round(config.origin[0]/config.gridSpaceing))
+    for lineValue in range(1,loopNumber+1):
+        lineX=config.origin[0]-config.gridSpaceing*lineValue
+
+        point1=(lineX,0)
+        point2=(lineX,config.screenSize[1])
+        pg.draw.aaline(gameDisplay,(255,0,0),point1,point2)
+
     #xAxis
     point1=(0,config.origin[1])
     point2=(config.screenSize[0],config.origin[1])
     pg.draw.aaline(gameDisplay,(0,0,255),point1,point2)
     
     #xGridLines
+    #positive
     loopNumber=int(round((screenSize[1]-config.origin[1])/config.gridSpaceing))
     for lineValue in range(1,loopNumber+1):
         lineY=config.origin[1]+config.gridSpaceing*lineValue
+
+        point1=(0,lineY)
+        point2=(config.screenSize[0],lineY)
+        pg.draw.aaline(gameDisplay,(255,0,0),point1,point2)
+    
+    #negative
+    loopNumber=int(round(config.origin[1]/config.gridSpaceing))
+    for lineValue in range(1,loopNumber+1):
+        lineY=config.origin[1]-config.gridSpaceing*lineValue
 
         point1=(0,lineY)
         point2=(config.screenSize[0],lineY)
@@ -76,7 +95,7 @@ def convertToFontDict(listOfLineSets):
 #leaving you with a list of all the sets
 #useful for inputting into graphing softwear
 #does not mutate input
-def convertToTups(listOfLineSets):
+def convertLineListsToTups(listOfLineSets):
     tupSets=[]
     for lineSet in listOfLineSets:
         tupSet=[]
@@ -87,11 +106,25 @@ def convertToTups(listOfLineSets):
             tupSet.append(point2)
         tupSets.append(tupSet)
     return(tupSets)
+
+def convertPointListsToTups(listOfPointLists):
+    tupSets=[]
+    for pointList in listOfPointLists:
+        tupSet=[]
+        for point in pointList:
+            tupSet.append((point[0],point[1]))
+        tupSets.append(tupSet)
+    return(tupSets)
+
 class Application:
     def __init__(self):
         self.hasQuit=False
-        #has
-        self.graphGuides=[[[[0, 4], [0, 0]], [[0, 0], [2, 0]], [[2, 0], [2, 4]], [[2, 4], [0, 4]]]]
+
+class LineListApplication(Application):
+    def __init__(self):
+        super().__init__()
+        
+        self.graphGuides=config.lineListGraphGuides
         #all lines are unconverted and will be converted when applicaiton is closed
         #submitting lines with submit them in a set
         self.submittedLines=[]
@@ -174,8 +207,95 @@ class Application:
                     displayLine=(inverseConvertCoords(line[0]),inverseConvertCoords(line[1]))
                     
                     pg.draw.aaline(gameDisplay,(0,255,0),displayLine[0],displayLine[1])
+    
+    def printResults(self):
+        print(self.submittedLines)
+        if config.convertToTups:
+            print(convertLineListsToTups(self.submittedLines))
+        if config.convertToFontDict:
+            print(convertToFontDict(self.submittedLines))    
+class PointListApplication(Application):
+    
+    
+    def __init__(self):
+        super().__init__()
+        self.graphGuides=config.pointListGraphGuides
+        #all lines are unconverted and will be converted when applicaiton is closed
+        #submitting lines with submit them in a set
+        self.submittedPointLists=[]
+        self.numberOfSubmittedPointLists=len(self.submittedPointLists)
+        self.unsubmittedPointList=[]
         
+        self.currentPointNumber=0
+    
+    def getAndApplyControls(self,gameDisplay):
+        for event in pg.event.get():
+            # checks if player has quit
+            if event.type == pg.QUIT:
+                self.hasQuit=True
 
+            #mouse click
+            if event.type == pg.MOUSEBUTTONDOWN:
+                mousePos=event.pos
+                button=event.button
+                #left click
+                if button==1:
+                    point=convertCoords(mousePos)
+                    self.unsubmittedPointList.append(point)
+                    self.currentPointNumber+=1
+
+                
+                #right click
+                elif button==3:
+                    if self.currentPointNumber!=0:
+                        del self.unsubmittedPointList[-1]
+                        self.currentPointNumber-=1
+
+            #keyboard controls
+            if event.type==pg.KEYDOWN:
+                #backspace
+                if event.key==pg.K_BACKSPACE:
+                    self.unsubmittedPointList=[]
+                    self.currentPointNumber=0
+
+                #enter
+                if event.key==pg.K_RETURN:
+                    self.numberOfSubmittedPointLists+=1
+                    self.submittedPointLists.append(self.unsubmittedPointList)
+                    self.unsubmittedPointList=[]
+                    self.currentPointNumber=0
+        
+        mousePos=[pg.mouse.get_pos()[0],pg.mouse.get_pos()[1]]
+        if self.currentPointNumber!=0:
+            point1=inverseConvertCoords(self.unsubmittedPointList[self.currentPointNumber-1])
+            pg.draw.aaline(gameDisplay,(255,255,255),point1,mousePos)
+
+    def renderUnsubmittedLines(self,gameDisplay):
+        if self.currentPointNumber>=2:
+            for i in range(0,len(self.unsubmittedPointList)-1):
+                point1=self.unsubmittedPointList[i]
+                point2=self.unsubmittedPointList[i+1]
+                displayLine=(inverseConvertCoords(point1),inverseConvertCoords(point2))
+                pg.draw.aaline(gameDisplay,(255,255,255),displayLine[0],displayLine[1])
+
+    def renderGraphGuides(self,gameDisplay):
+        for pointList in self.graphGuides:
+            transformedPointList=[]
+            for point in pointList:
+                transformedPointList.append(inverseConvertCoords(point))
+            pg.draw.aalines(gameDisplay,(0,255,0),True,transformedPointList)
+
+        if config.displayPrevious:
+            for pointList in self.submittedPointLists:
+                transformedPointList=[]
+                for point in pointList:
+                    transformedPointList.append(inverseConvertCoords(point))
+                pg.draw.aalines(gameDisplay,(0,255,0),True,transformedPointList)
+
+    def printResults(self):
+        print(self.submittedPointLists)
+        if config.convertToTups:
+            print(convertPointListsToTups(self.submittedPointLists))
 
 if config.convertToFontDict:
     print('Current Letter: A')
@@ -185,7 +305,14 @@ gameDisplay = pg.display.set_mode((config.screenSize[0], config.screenSize[1]))
 pg.init()
 pg.display.set_caption("")
 clock = pg.time.Clock()
-appInstance=Application()
+if config.mode==0:
+    appInstance=LineListApplication()
+elif config.mode==1:
+    appInstance=PointListApplication()
+    if config.convertToFontDict:
+        print("cannot make font dict with point list")
+else:
+    print("mode Error")
 
 while not appInstance.hasQuit:
     pg.Surface.fill(gameDisplay,(0,0,0))
@@ -196,9 +323,4 @@ while not appInstance.hasQuit:
     clock.tick_busy_loop(frameRate)
     pg.display.update()
 
-#output
-print(appInstance.submittedLines)
-if config.convertToTups:
-    print(convertToTups(appInstance.submittedLines))
-if config.convertToFontDict:
-    print(convertToFontDict(appInstance.submittedLines))
+appInstance.printResults()
